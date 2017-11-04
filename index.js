bdf0738d89ad06198407f5b457c6383f6bfaf468
Elliot Kahan <ejkahan@gmail.com>
2017-11-04 20:24:40 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+var a
+var b 
+a = Math.floor(Math.random() * 1000)
+b = Math.floor(Math.random() * 1000)
+
+function add(a, b) {
+return  (a + b)
+}
+
+
 function makeInt(n) {
     parseInt('n', 10)
 }
